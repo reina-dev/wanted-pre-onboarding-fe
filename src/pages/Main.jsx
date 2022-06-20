7c836329d8d87604a9087ad18ee6384982b1957b
YounJoo Seog <reina.seog@gmail.com>
2022-06-21 02:00:09 +0900

    [Add] Main Page 구현중

---
+++ src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <h2>hello</h2>
+    </div>
+  );
+};
+
+export default Main;
